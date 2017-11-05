832fdbd66e60b209783254256eba7debb4afd32e
Prakhar Rastogi <prakharrastogi007@gmail.com>
2017-11-05 15:24:19 +0530

    hero search added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,37 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }   from '@angular/router';
-
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroService }         from './hero.service';
-
-import { AppRoutingModule }     from './app-routing.module';
-
+import { HttpModule }    from '@angular/http';
+ 
+import { AppRoutingModule } from './app-routing.module';
+ 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+ 
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent } from './hero-search.component';
+import { HeroService }          from './hero.service';
+ 
 @NgModule({
-  imports: [ BrowserModule, 
-             FormsModule,
-             AppRoutingModule 
-          ],
-  declarations: [ AppComponent, 
-    HeroDetailComponent, 
-    HeroesComponent, 
-    DashboardComponent 
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
   ],
-  providers:    [ HeroService ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
